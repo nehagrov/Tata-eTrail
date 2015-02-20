25be0395458634dd7f6b895ba117434b2d3e952d
131005 <dbhargava@ezibuy.co.nz>
2015-02-20 17:48:27 +0530

    Commit

---
+++ training/gensrc/org/training/constants/GeneratedTrainingConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 20 Feb, 2015 1:56:46 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package org.training.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingConstants
+{
+	public static final String EXTENSIONNAME = "training";
+	
+	protected GeneratedTrainingConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
